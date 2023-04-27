d6c4beec66f45a79849baa6c339f9d51a5a8a5b8
z00texnik <85977482+z00texnik@users.noreply.github.com>
2023-04-27 17:16:17 +0700

    Update Head.py

--- Head.py
+++ Head.py
@@ -2,7 +2,7 @@ import logging
 import time
 from jira import JIRA
 import telebot
-from auth_data import token, url, username, password
+from auth_data import token, username, password
 from telebot import types
 
 options= {
@@ -10,17 +10,17 @@ options= {
     'verify': True
 }
 bot = telebot.TeleBot(token)
-keybo = telebot.types.ReplyKeyboardMarkup(True)
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+start=types.KeyboardButton('start')
 au_bjra = JIRA(options=options, basic_auth=(username, password))  # переменная отвечающая за авторизацию в жиру
 
 
-@bot.message_handler(
-    commands=['start'])  # создаётся обработчик команды "start" и ответ на команду + возвращает имя написавшего
+@bot.message_handler(commands=['start'])  # создаётся обработчик команды "start" и ответ на команду + возвращает имя написавшего
 def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item = types.KeyboardButton(text='Статус задачи')
-    markup.add(item)
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
+    start = types.KeyboardButton('Создать задачу')
+    markup.add(start)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!', reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])  # создаётся обработчик команды "help" и ответ на команду
@@ -32,40 +32,37 @@ def help(message):
 #    content_types=['text'])  # в связке с командой issue, которая по введённому номеру возвращает статус задачи
 #def handle_text(message):
 #    bot.send_message(message.chat.id, 'Статус задачи: ' + au_bjra.get_issue_status(message.text))
-issue_dict = {
-    'project': {'key': 'ARN'},
-    'summary': 'Имя задачи',
-    'description': 'Описание задачи',
-    'issuetype':{'name':'Баг'},
-    'labels':['Внутреннее'],
-    'subsystem':{'name':'Backend'}
-}
 
-def create_newIssue(issueName, descr, issueType, labels, subsystemsName):
+def create_newIssue(projectKey, issueName, descr, issueType, labels):
   issue_dict = {
+  'project':{'key': projectKey},
   'summary': issueName,
   'description': descr,
   'issuetype':{'name': issueType},
   'labels':[labels],
-  'subsystem':{'name': subsystemsName}
 }
   return issue_dict
 
-#newissue= au_bjra.create_issue(fields=issue_dict)
-@bot.message_handler(commands=['new_issue'])
-def new_issue(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    newIssue=types.KeyboardButton('Завести задачу')
-    markup.add(newIssue)
-    bot.send_message(message.chat.id, 'Вводи данные по задаче по очереди: Название задачи, Описание задачи, Тип задачи, SubSystem, Лейбл(ы)')
 
+
+#newissue= au_bjra.create_issue(fields=issue_dict)
 @bot.message_handler(content_types=['text'])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Название задачи: ' + au_bjra(create_newIssue(issueName=message.text)) + au_bjra(create_newIssue(descr=message.text)) + au_bjra(create_newIssue(issueType=message.text)) + au_bjra(create_newIssue(subsystemsName=message.text)) + au_bjra(create_newIssue(labels=message.text))).split
-#    bot.send_message(message.chat.id, 'Описание задачи: ' + au_bjra(create_newIssue(descr=message.text)))
-#    bot.send_message(message.chat.id, 'Тип задачи: ' + au_bjra(create_newIssue(issueType=message.text)))
-#    bot.send_message(message.chat.id, 'SubSystem: ' + au_bjra(create_newIssue(subsystemsName=message.text)))
-#    bot.send_message(message.chat.id, 'Лейблы: ' + au_bjra(create_newIssue(labels=message.text)))
-    return handle_text
+def new_issue(message):
+    if message.text == 'Создать задачу':
+        bot.send_message(message.chat.id, 'Вводи данные по задаче по очереди: Название проекта, Название задачи, Описание задачи, Тип задачи, Лейбл(ы)')
+        issue_fuck = message.text
+        issue_fuck = issue_fuck.split('\n')
+    b = create_newIssue(issue_fuck[0], issue_fuck[1], issue_fuck[2], issue_fuck[3], issue_fuck[4])
+    au_bjra.create_issue(fields=b)
+    bot.send_message(message.chat.id, 'Задача заведена')
+
+
+#@bot.message_handler(content_types=['text'])
+#def handle_text(message):
+#    issue_fuck=message.text
+#    issue_fuck=issue_fuck.split('\n')
+#    b=create_newIssue(issue_fuck[0], issue_fuck[1], issue_fuck[2], issue_fuck[3], issue_fuck[4])
+#    au_bjra.create_issue(fields=b)
+
 
 bot.infinity_polling()  # бесконечный запуск
